ec9cd716a499097d1eedd3396cd7dcf50532f936
Diego Martin <diegomtnfranco@gmail.com>
2026-05-16 17:32:53 -0300

    conf: se instala dep de docker y cambios en .env

--- frontend/src/features/auth/services/auth.service.ts
+++ frontend/src/features/auth/services/auth.service.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 import type { LoginPayload } from '../types/auth.types'
 
-const API_URL = 'http://localhost:3000'
+const API_URL = 'http://localhost:3000/api'
 
 export const login = async (payload: LoginPayload) => {
   const response = await axios.post(`${API_URL}/auth/login`, payload)
