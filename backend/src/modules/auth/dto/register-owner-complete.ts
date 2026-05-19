dfc064d5b42c11c5a1569dc7841eece145bcb303
FranciscoTorres <franciscot.p.93@gmail.com>
2026-05-19 20:51:57 -0300

    feat: - Se agrega nuevo servicio de auth, se agrega nuevo endpoint para el registro completo de el owner con su parking y spaces seteados:

--- backend/src/modules/auth/dto/register-owner-complete.ts
+++ backend/src/modules/auth/dto/register-owner-complete.ts
@@ -1,12 +1,12 @@
 // src/auth/dto/register-owner-complete.dto.ts
 import { 
-  IsEmail, IsString, MinLength, IsPhoneNumber, IsOptional, 
+  IsEmail, IsString, MinLength, IsOptional, 
   IsLatitude, IsLongitude, IsMilitaryTime, 
-  IsNumber, Min, Max, IsBoolean, IsArray, 
-  MaxLength
+  IsNumber, Min, Max, IsBoolean, 
+  MaxLength,
+  Matches
 } from 'class-validator';
-import { IsCUIT } from '../../parking-owners/common/decorators/is-cuit.decorator'; // Crearemos este decorador
-import { VehicleType } from '../../common/enums/vehicle-type.enum';
+import { IsCUIT } from '../../parking-owners/common/decorators/is-cuit.decorator';
 
 export class RegisterOwnerCompleteDto {
   // Datos del usuario
@@ -15,20 +15,23 @@ export class RegisterOwnerCompleteDto {
 
   @IsString()
   @MinLength(8, { message: 'La contraseña debe tener al menos 8 caracteres' })
+  @MaxLength(50, { message: 'La contraseña no puede tener más de 50 caracteres' })
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, { message: 'La contraseña debe contener al menos una letra mayúscula, una letra minúscula, un número y un carácter especial' })  
   password!: string;
 
   // Datos del dueño
+  
   @IsString()
-  @MinLength(3)
-  @MaxLength(80)
+  @MinLength(3, { message: 'El nombre debe tener al menos 3 caracteres' })
+  @MaxLength(80, { message: 'El nombre no puede tener más de 80 caracteres' })
   name!: string;
 
   @IsString()
-  @MinLength(3)
-  @MaxLength(100)
+  @MinLength(3, { message: 'El nombre comercial debe tener al menos 3 caracteres' })
+  @MaxLength(100, { message: 'El nombre comercial no puede tener más de 100 caracteres' })
   businessName!: string;
 
-  @IsCUIT({ message: 'Debe proporcionar un CUIT válido' })
+  @IsCUIT({ message: 'Debe proporcionar un CUIT válido (formato: XX-XXXXXXXX-X)' })
   @IsOptional()
   cuit?: string;
 
@@ -40,7 +43,7 @@ export class RegisterOwnerCompleteDto {
   @IsOptional()
   address?: string;
 
-  // Datos del estacionamiento
+  // Datos del estacionamiento (primer parking)
   @IsString()
   @IsOptional()
   parkingName?: string;
@@ -59,19 +62,10 @@ export class RegisterOwnerCompleteDto {
 
   @IsNumber()
   @Min(1)
-  @Max(200)
+  @Max(150,{ message: 'El número máximo de espacios permitido es 150' })
   totalSpaces!: number;
 
   @IsBoolean()
   @IsOptional()
   allowOnlineReservations?: boolean;
-
-  // Distribución de espacios (opcional, si no se envía se calcula automáticamente)
-  @IsOptional()
-  spacesDistribution?: {
-    car: number;
-    truck: number;
-    motorcycle: number;
-    van?: number;
-  };
 }
