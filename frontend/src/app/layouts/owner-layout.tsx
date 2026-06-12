ca9fa78651e497784836827d93841dea6351179b
Diego Martin <diegomtnfranco@gmail.com>
2026-06-12 00:42:09 -0300

    S-55 Optimizar layout mobile y refactorizar Sidebar a Navbar (Admin y Parking Owner)

--- frontend/src/app/layouts/owner-layout.tsx
+++ frontend/src/app/layouts/owner-layout.tsx
@@ -8,12 +8,17 @@ function OwnerLayout() {
   return (
     <div className="min-h-screen flex flex-col bg-gray-100">
       <Navbar />
-      <div className= "  md:flex flex-1 w-full gap-4 px-4 py-4">
-        <SideBarMenu />
+
+      <div className="md:flex flex-1 w-full gap-4 px-4 py-4">
+        <div className="hidden md:block">
+          <SideBarMenu />
+        </div>
+
         <div className="flex-1 rounded-lg bg-slate-50 p-4 shadow-sm">
           <Outlet />
         </div>
       </div>
+
       <Footer />
     </div>
   )
