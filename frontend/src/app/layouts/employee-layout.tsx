607c320af799d929e731e87f8fbb75141ea2c8c2
Tinza <JMZAM2009@GMAIL.COM>
2026-06-26 21:16:20 -0300

    Característica: incorporación de EmployeeLayout y SideBarMenuEmployee para el enrutamiento y el diseño específicos de los empleados
    - Se ha creado el componente EmployeeLayout para estructurar las páginas de los empleados con barra de navegación, barra lateral y pie de página.
    - Se ha implementado SideBarMenuEmployee para la navegación específica de los roles de los empleados.
    - Se ha actualizado AppRouter para enrutar las rutas de los empleados a EmployeeLayout.
    - Se han modificado QRManagementPage y RatesPage para adaptarlas al acceso y la funcionalidad de los roles de los empleados.
    - Se ha añadido EmployeeMenu para definir los elementos de navegación de los empleados.

--- frontend/src/app/layouts/employee-layout.tsx
+++ frontend/src/app/layouts/employee-layout.tsx
@@ -1,18 +1,18 @@
 import { Outlet } from 'react-router-dom'
 
 import Navbar from '../../shared/components/Navbar'
-import SideBarMenu from '../../shared/components/SideBarMenu'
-  
+// import SideBarMenu from '../../shared/components/SideBarMenu'
+import SideBarMenuEmployee from '../../shared/components/SideBarMenuEmployee'
 import Footer from '../../shared/components/Footer'
 
-function OwnerLayout() {
+function EmployeeLayout() {
   return (
     <div className="min-h-screen flex flex-col bg-gray-100">
       <Navbar />
 
       <div className="md:flex flex-1 w-full gap-4 px-4 py-4">
         <div className="hidden md:block">
-          <SideBarMenu />
+          <SideBarMenuEmployee />
         </div>
 
         <div className="flex-1 rounded-lg bg-slate-50 p-4 shadow-sm">
@@ -25,4 +25,4 @@ function OwnerLayout() {
   )
 }
 
-export default OwnerLayout
+export default EmployeeLayout
