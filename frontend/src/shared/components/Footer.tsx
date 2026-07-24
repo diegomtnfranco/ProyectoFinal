4b77174e583a4eeb06f77287e0888d43a1d9c7cd
FranciscoTorres <franciscot.p.93@gmail.com>
2026-07-23 21:53:46 -0300

    Feat: Se agrega contenido a la landing y se modifica el footer

--- frontend/src/shared/components/Footer.tsx
+++ frontend/src/shared/components/Footer.tsx
@@ -1,4 +1,3 @@
-// frontend/src/shared/components/layouts/Footer.tsx
 const Footer = () => {
   return (
     <footer className="w-full border-t border-slate-200 bg-white py-2 text-center text-xs text-slate-400 mt-auto">
